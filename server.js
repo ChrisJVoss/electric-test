20296f5c1560ef5804c689a0ada3951b81622782
Chris Voss <chrisvoss92@gmail.com>
2017-08-16 16:14:04 -0700

    changed scope.

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ pem.createCertificate({days: 1, selfSigned: true }, function(err, keys){
   https.createServer({key: keys.serviceKey, cert: keys.certificate}, app).listen(3000, () => {
     console.log('Listening on 3000')
   })
-})
-const io = require('socket.io')(https)
-const path = require('path')
-const SimplePeer = require('simple-peer')
+  const io = require('socket.io')(https)
+  const path = require('path')
+  const SimplePeer = require('simple-peer')
 
-let connections = []
+  let connections = []
 
-io.on('connection', socket => {
-  connections.push(socket)
-  console.log('%s users connected', connections.length)
-  socket.on('yourId', (id) => {
-    socket.broadcast.emit('otherId', id)
-    console.log(id)
-  })
-  socket.on('disconnect', function() {
-    connections.splice(connections.indexOf(socket), 1)
+  io.on('connection', socket => {
+    connections.push(socket)
     console.log('%s users connected', connections.length)
+    socket.on('yourId', (id) => {
+      socket.broadcast.emit('otherId', id)
+      console.log(id)
+    })
+    socket.on('disconnect', function() {
+      connections.splice(connections.indexOf(socket), 1)
+      console.log('%s users connected', connections.length)
+    })
   })
 })
